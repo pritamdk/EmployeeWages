28d826a88d7551be12ee687ac4684f290446b54f
pritamdk <pritamdk1994@gmail.com>
2022-06-20 01:26:52 +0530

    Added : Attendance check UC1

--- employeewages/EmployeeWages.java
+++ employeewages/EmployeeWages.java
@@ -4,8 +4,13 @@ public class EmployeeWages {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Employee Wage Computation Program");
-
+		int isFullTime=1;
+		
+		double empCheck=Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isFullTime)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 
 }
