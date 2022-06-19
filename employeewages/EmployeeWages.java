a2babbd1c1296041fdfd513e098173dd1d4f68c8
pritamdk <pritamdk1994@gmail.com>
2022-06-20 01:47:44 +0530

    Added : UC2- Daily Employee Wage

--- employeewages/EmployeeWages.java
+++ employeewages/EmployeeWages.java
@@ -1,16 +1,23 @@
 package com.employeewages;
 
+import java.util.Random;
+
 public class EmployeeWages {
 
 	public static void main(String[] args) {
 		
-		int isFullTime=1;
-		
-		double empCheck=Math.floor(Math.random() * 10) % 2;
-		if (empCheck == isFullTime)
-			System.out.println("Employee is present");
-		else
-			System.out.println("Employee is absent");
-	}
-
+		int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        
+        int empHrs = 0;
+        int empWage = 0;
+        
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            empHrs = 8;
+        else
+            empHrs = 8;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Daily Wage: " + empWage);
+    }
 }
