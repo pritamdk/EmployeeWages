9cb5c9e5083c036f64d85fc05fc1e224590af48f
pritamdk <pritamdk1994@gmail.com>
2022-06-20 01:11:17 +0530

    Added: Welcome to EmployeeWages Program

---
+++ employeewages/EmployeeWages.java
@@ -0,0 +1,11 @@
+package com.employeewages;
+
+public class EmployeeWages {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+	}
+
+}
