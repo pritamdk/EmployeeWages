55d56d1deec24115fa1ec600acfc2b50e8d74a5d
pritamdk <pritamdk1994@gmail.com>
2022-06-20 16:49:28 +0530

    Added: UC5- Monthly Wages

--- employeewages/EmployeeWages.java
+++ employeewages/EmployeeWages.java
@@ -1,30 +1,54 @@
 package com.employeewages;
 
+import java.util.Random;
+
 public class EmployeeWages {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
 
-	    public static final int IS_PART_TIME = 1;
-	    public static final int IS_FULL_TIME = 2;
-	    public static final int EMP_RATE_PER_HOUR = 20;
+		Random random = new Random();
+		int attendance = random.nextInt(3); // generate random number 0,1,2
 
-	    public static void main (String[] args)
-	    {
-	        
-	        int empHrs = 0;
-	        int empWage = 0;
-	        
-	        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-	        switch (empCheck)
-	        {
-	            case IS_PART_TIME:
-	                empHrs = 4;
-	                break;
-	            case IS_FULL_TIME:
-	                empHrs = 8;
-	                break;
-	            default:
-	                empHrs = 0;
-	        }
-	        empWage = empHrs * EMP_RATE_PER_HOUR;
-	        System.out.println("Employee Wage: " + empWage);
-	    }
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int partTimeHours = 4;
+		int wagePerDay = 0;
+		int monthlySalary = 0;
+
+		switch (attendance) {
+		case 0:
+			System.out.println("Employee Absent ..");
+			break;
+		case 1:
+			wagePerDay = partTimeHours * wagePerHour;
+			break;
+		default:
+			wagePerDay = fullDayHour * wagePerHour;
+		for (int day = 1; day <= 20; day++) 
+		{
+			int attendance1 = random.nextInt(3); // generate random number 0,1,2
+			System.out.println("randomNumber" + attendance1);
+			wagePerDay = 0;
+
+			switch (attendance1) {
+			case 0:
+				System.out.println("Employee Absent ..");
+				break;
+			case 1:
+				System.out.println("part time present");
+				wagePerDay = partTimeHours * wagePerHour;
+				break;
+			default:
+				System.out.println("Full day present");
+				wagePerDay = fullDayHour * wagePerHour;
+				break;
+			}
+			monthlySalary = monthlySalary + wagePerDay;
+			System.out.println("Day " + day + "  : " + "Monthly Salary " + monthlySalary);
+		}
+
+		System.out.println("Employee wage per Day: " + wagePerDay);
 	}
+}
+}
